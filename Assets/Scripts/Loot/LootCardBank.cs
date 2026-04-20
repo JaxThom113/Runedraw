aa246e6b576932a778022bd40ec2a5ba1c39887a
elijahlowe77 <92341419+elijahlowe77@users.noreply.github.com>
2026-04-20 14:21:39 -0400

    Script changes

--- Assets/Scripts/Loot/LootCardBank.cs
+++ Assets/Scripts/Loot/LootCardBank.cs
@@ -21,11 +21,25 @@ public class LootCardBank : MonoBehaviour
         return chosenCards;
     }
 
-    public List<CardSO> GetRandomCardsEnemy(CardSO ultimateCard)
+    public List<CardSO> GetRandomCardsEnemy(CardSO ultimateCard, int amount)
     {
         List<CardSO> chosenCards = new List<CardSO>();
-        if (ultimateCard != null)
-            chosenCards.Add(ultimateCard);
+        CardSO firstCard = cards[Random.Range(0, cards.Count)];
+        chosenCards.Add(firstCard);
+        chosenCards.Add(ultimateCard);
+
+        CardSO thirdCard = cards[Random.Range(0, cards.Count)];
+        if (thirdCard == firstCard || thirdCard == ultimateCard)
+        {
+            int nextIndex = (cards.IndexOf(thirdCard) + 1) % cards.Count;
+            thirdCard = cards[nextIndex];
+            if (thirdCard == firstCard || thirdCard == ultimateCard)
+            {
+                thirdCard = cards[(nextIndex + 1) % cards.Count];
+            }
+        }
+        chosenCards.Add(thirdCard);
+
         return chosenCards;
     }
 }
